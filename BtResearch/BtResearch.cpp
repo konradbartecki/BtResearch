f2f2416f8879f515f3d4daa55e608bfd2779ebc0
Konrad Bartecki <konrad.bartecki@predica.pl>
2016-04-13 20:55:13 +0200

    Added SOCKADDR_BTH

--- BtResearch/BtResearch.cpp
+++ BtResearch/BtResearch.cpp
@@ -19,6 +19,17 @@ int main()
 		return 1;
 	}
 
+	struct addrinfo *result = NULL,
+		*ptr = NULL,
+		hints;
+
+	ZeroMemory(&hints, sizeof(hints));
+	hints.ai_family = AF_BTH;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_protocol = BTHPROTO_RFCOMM;
+
+	SOCKADDR_BTH bluetoothAddress;
+	bluetoothAddress.addressFamily = 
 
     return 0;
 }
