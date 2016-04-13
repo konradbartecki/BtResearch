537aa1d30bf04049b2483815439dcce5c3b93f74
Konrad Bartecki <konrad.bartecki@predica.pl>
2016-04-13 21:07:54 +0200

    Added includes

--- main.cpp
+++ main.cpp
@@ -1,7 +1,22 @@
+#include <Windows.h>
+#include <WinSock2.h>
 #include <iostream>
+#include <ws2bth.h>
+
 using namespace std;
 
 int main() {
+	
+	struct addrinfo *result = NULL,
+		*ptr = NULL,
+		hints;
+
+	ZeroMemory(&hints, sizeof(hints));
+	hints.ai_family = AF_BTH;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_protocol = BTHPROTO_RFCOMM;
+
+
 	std::cout << "Siema Konrad";
 	return 0;
 }
